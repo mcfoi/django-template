bc69e926bcf8a96dc6a4c601a6b93a5dd2677d52
Marco Foi <foimarco@gmail.com>
2015-08-27 20:32:07 +0000

    Postgres port changed to 5432

--- config/settings/base.py
+++ config/settings/base.py
@@ -86,7 +86,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
-        'PORT': '5434',
+        'PORT': '5432',
     },
     'basesqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
